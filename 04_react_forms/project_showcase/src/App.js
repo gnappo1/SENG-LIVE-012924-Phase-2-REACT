87a879eab3bbacbeb606b22535b181a5d42c6782
Matteo Piccini <gnappo.1@live.it>
2024-02-22 13:11:51 -0800

    end of lecture code

--- 04_react_forms/project_showcase/src/App.js
+++ 04_react_forms/project_showcase/src/App.js
@@ -4,6 +4,7 @@ import ProjectForm from "./components/project/ProjectForm";
 import ProjectList from "./components/project/ProjectList";
 import SearchBar from "./components/search/SearchBar";
 import ButtonsFilter from "./components/search/ButtonsFilter";
+import { v4 as uuidv4 } from "uuid"
 
 const App = () => {
   //! LOCAL STATE
@@ -13,8 +14,21 @@ const App = () => {
   const [projects, setProjects] = useState([]);
   const [searchQuery, setSearchQuery] = useState("")
   const [phaseSelected, setPhaseSelected] = useState("All");
+  const [error, setError] = useState("");
 
-  
+  //! CRUD
+  const addProject = (newProject) => {
+    setProjects(currentProjects => {
+      const lastProjectArray = currentProjects.slice(-1)
+      const id = lastProjectArray.length ? Number(lastProjectArray[0].id) + 1 : uuidv4()
+      return [...currentProjects, {...newProject, id}]
+    })
+  }
+
+  const removeLastProject = () => {
+    setProjects(currentProjects => currentProjects.slice(0, -1))
+  }
+
   const handlePhaseSelection = (e) => {
     if (e.target.textContent === "All") {
       setPhaseSelected("All")
@@ -33,15 +47,20 @@ const App = () => {
   const loadProjects = () => {
     fetch("http://localhost:4000/projects")
     .then((res) => res.json())
-    .then((projects) => setProjects(projects));
+    .then((projects) => setProjects(projects))
+    .catch(err => {
+      setError(err.message)
+      setTimeout(() => setError(""), 5000)
+    });
   }
 
   const toggleDarkMode = () => setIsDarkMode(!isDarkMode)
 
   return (
     <div className={isDarkMode ? "App" : "App light"}>
+      {error ? <p className="error-message red">{error}</p> : null}
       <Header isDarkMode={isDarkMode} onToggleDarkMode={toggleDarkMode} />
-      <ProjectForm />
+      <ProjectForm addProject={addProject} removeLastProject={removeLastProject } />
       <button className="load-btn" onClick={loadProjects}>Load Projects</button>
       <ButtonsFilter handlePhaseSelection={handlePhaseSelection}/>
       <SearchBar handleSearch={handleSearch} searchQuery={searchQuery} />
