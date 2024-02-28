cf85050c7a16b0b86c00025072a2f59139f29208
Matteo Piccini <gnappo.1@live.it>
2024-02-28 11:47:20 -0800

    add useMemo logic

--- 08_bonus_hooks/project_showcase/src/components/project/ProjectList.js
+++ 08_bonus_hooks/project_showcase/src/components/project/ProjectList.js
@@ -1,26 +1,29 @@
 import ProjectListItem from "./ProjectListItem";
+import { useState, useMemo } from "react";
 
 const ProjectList = ({projects, searchQuery, phaseSelected, setEditingModeId, handleDelete}) => {
 
-    const renderProjects = () => {
-      return finalProjects.map(project => (
+  const [test, setTest] = useState(0);
+
+    const renderProjects = useMemo(() => projects
+        .filter(project => {
+          console.log("I am recalculating the collection")
+          return (phaseSelected === "All" || project.phase === phaseSelected) && (project.name.toLowerCase().includes(searchQuery.toLowerCase()))
+        })
+        .map(project => (
         <ProjectListItem
         key={project.id}
         setEditingModeId={setEditingModeId}
         handleDelete={handleDelete}
         {...project}
         />
-        ))
-      }
-          
-    const finalProjects = projects
-    .filter(project => {
-      return (phaseSelected === "All" || project.phase === phaseSelected) && (project.name.toLowerCase().includes(searchQuery.toLowerCase()))
-    })
+        )), [phaseSelected, searchQuery, projects, setEditingModeId, handleDelete])
+
   return (
     <section>
       <h2>Projects</h2>
-      <ul className="cards">{renderProjects()}</ul>
+      <button onClick={() => setTest(current => current + 1)}>Click me!</button>
+      <ul className="cards">{renderProjects}</ul>
     </section>
   );
 };
